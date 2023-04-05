3fc1e861a8ee68a12dd55eaabae1b44cab86cf2d
Adholah96 <adholah96@gmail.com>
2023-04-05 16:22:45 +0300

    Fix bugs in Book.js

--- src/Book.js
+++ src/Book.js
@@ -1,16 +1,28 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from './redux/books/bookSlice';
 
-const Book = ({ author, title }) => (
-  <p>
-    {title}
-    -
-    {author}
-    <button type="button">Remove</button>
-  </p>
-);
+const Book = ({ item }) => {
+  const dispatch = useDispatch();
+  const { title, author, item_id: id } = item;
+
+  return (
+    <>
+      {title}
+      -
+      {author}
+      <button type="button" onClick={() => dispatch(removeBook(id))}>
+        Remove
+      </button>
+    </>
+  );
+};
 
 Book.propTypes = {
-  author: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
+  item: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    item_id: PropTypes.string.isRequired,
+  }).isRequired,
 };
 export default Book;
