0e1314e79d29a77e6f1125b213fb9bc0101c533c
Adholah96 <adholah96@gmail.com>
2023-04-06 14:33:34 +0300

    Fetch data from API to be displayed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,52 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Form from './Form';
 import Book from '../Book';
+import { fetchBooksData } from '../redux/books/bookSlice';
 
 function Home() {
-  const { bookItems } = useSelector((store) => store.book);
+  const {
+    books, isLoading, error, success,
+  } = useSelector(
+    (store) => store.book,
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchBooksData());
+  }, [dispatch, success]);
+
+  if (isLoading) {
+    return <h1>Waiting</h1>;
+  }
+
+  if (error) {
+    return <h2>Unable to fetch books</h2>;
+  }
+
+  if (books.length === 0) {
+    return (
+      <>
+        <h2 className="no-book">No books, Add a few</h2>
+        <Form />
+      </>
+    );
+  }
+
   return (
     <>
       <div className="container">
         <div className="booksContainer">
           <ul className="booksul">
-            {bookItems.map((book) => (
-              <li key={book.item_id}>
+            {books.map((book) => (
+              <li key={book.id}>
                 <Book item={book} />
               </li>
             ))}
           </ul>
         </div>
-        <Form />
       </div>
+      <Form />
     </>
   );
 }
