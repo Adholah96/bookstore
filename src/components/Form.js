d8496178f4512555ff7530c5a8b9e0c1b1d76759
Adholah96 <adholah96@gmail.com>
2023-04-06 14:32:41 +0300

    Make changes to Form to add data to the API

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,56 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/bookSlice';
 
 const Form = () => {
+  const [input, setInput] = useState({
+    title: '',
+    author: '',
+  });
   const dispatch = useDispatch();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const random = () => {
-      const number = Math.floor(Math.random() * 44556) + 10000;
-      const random = number.toString();
-      return random;
-    };
-    const formData = new FormData(e.target);
-    const newBook = {
-      item_id: random(),
-      author: formData.get('author'),
-      title: formData.get('title'),
-    };
-    dispatch(addBook(newBook));
-    e.target.reset();
+
+  const handleInputChange = (e) => {
+    setInput((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+    const book = { id, ...input };
+    dispatch(addBook(book));
+    setInput(input);
+    setInput({
+      title: '',
+      author: '',
+    });
+  };
+
   return (
-    <div className="booksForm">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="title">
-          Title :
-          <input type="text" name="title" id="title" />
-        </label>
-        <label htmlFor="author">
-          Author :
-          <input type="text" name="author" id="author" />
-        </label>
-        <button type="submit">Add Book</button>
-      </form>
-    </div>
+    <form>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        name="title"
+        value={input.title}
+        placeholder="Title"
+      />
+      <br />
+      <input
+        type="text"
+        name="author"
+        onChange={handleInputChange}
+        value={input.author}
+        placeholder="Author"
+      />
+      <br />
+      <button type="button" onClick={handleFormSubmit}>
+        Add Book
+      </button>
+    </form>
   );
 };
 
